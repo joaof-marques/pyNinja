d4aadb4ecbbe1d7c29b3beab0fa31896d5f94f62
João Marques <joaofmarques.dev@gmail.com>
2024-01-11 18:32:02 -0300

    Adicionado método que renderiza paisagem

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 import pygame
+import os
 
-class Jogo:
+class Game:
     def __init__(self) -> None:
-        self.largura_tela = 800
-        self.altura_tela = 400
-        self.tela = pygame.display.set_mode((self.largura_tela, self.altura_tela))
+        self.screen_width = 1440
+        self.screen_height = 900
+        self.ground_level = 800
+        self.screen = pygame.display.set_mode((self.screen_width, self.screen_height))
         self.fps = pygame.time.Clock()
+    
+    def set_background(self) -> None:
+        background_surface = pygame.image.load(os.path.join("sprites", "background.png")).convert_alpha()
+        ground_surface = pygame.image.load(os.path.join("sprites", "ground.png")).convert_alpha()
+        self.screen.blit(background_surface, (0,0))
+        self.screen.blit(ground_surface, (0,self.ground_level-20))
     
     def run (self):
         pygame.init()
@@ -16,7 +24,10 @@ class Jogo:
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     exit()
+                    
+            self.set_background()
+            pygame.display.update()
 
 if __name__ == '__main__':
-    jogo = Jogo()
-    jogo.run()
+    game = Game()
+    game.run()
