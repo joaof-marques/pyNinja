963053a31b78d83fb2909a6f7cb98ea7aec4190e
João Marques <joaofmarques.dev@gmail.com>
2024-01-11 19:42:50 -0300

    Adicionado método que renderiza o herói e excluído antigos arquivos de sprites fora da estrutura

--- main.py
+++ main.py
@@ -8,12 +8,33 @@ class Game:
         self.ground_level = 800
         self.screen = pygame.display.set_mode((self.screen_width, self.screen_height))
         self.fps = pygame.time.Clock()
+        
+        self.hero_animation_frame = 0
+        
     
     def set_background(self) -> None:
-        background_surface = pygame.image.load(os.path.join("sprites", "background.png")).convert_alpha()
-        ground_surface = pygame.image.load(os.path.join("sprites", "ground.png")).convert_alpha()
+        background_surface = pygame.image.load(os.path.join("sprites","enviroment", "background.png")).convert_alpha()
+        ground_surface = pygame.image.load(os.path.join("sprites","enviroment", "ground.png")).convert_alpha()
         self.screen.blit(background_surface, (0,0))
         self.screen.blit(ground_surface, (0,self.ground_level-20))
+    
+    def get_frame_from_spritesheet (self, spritesheet, frame_size, frame_pos) -> pygame.Surface:        
+        x_start = frame_size*frame_pos
+        x_final = x_start + frame_size
+        
+        image = pygame.Surface((frame_size, frame_size), pygame.SRCALPHA)     
+        image.blit(spritesheet.convert_alpha(), (0,0), (x_start, 0, x_final, frame_size))
+        
+        return image
+    
+    
+    def draw_hero(self) -> None: 
+        hero_surface = pygame.image.load(os.path.join("sprites", 'hero', "hero_idle.png")).convert_alpha()
+        hero_frame_surface = self.get_frame_from_spritesheet(hero_surface, 128, self.hero_animation_frame//5).convert_alpha()
+        hero_rect = hero_frame_surface.get_rect(midbottom = (700, self.ground_level))
+        self.screen.blit(hero_frame_surface, hero_rect)
+        
+        
     
     def run (self):
         pygame.init()
@@ -26,6 +47,10 @@ class Game:
                     exit()
                     
             self.set_background()
+            if self.hero_animation_frame >= 30:
+                self.hero_animation_frame = 0
+            self.draw_hero()
+            self.hero_animation_frame += 1
             pygame.display.update()
 
 if __name__ == '__main__':
