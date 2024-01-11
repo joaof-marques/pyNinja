19e7adcef54ec943d63860e5147548049d9b3b6e
João Marques <joaofmarques.dev@gmail.com>
2024-01-11 17:34:12 -0300

    Criação da classe Jogo
    Tela funcionando

---
+++ main.py
@@ -0,0 +1,20 @@
+import pygame
+
+class Jogo:
+    def __init__(self) -> None:
+        self.largura_tela = 800
+        self.altura_tela = 400
+        self.tela = pygame.display.set_mode((self.largura_tela, self.altura_tela))
+    
+    def run (self):
+        pygame.init()
+        
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    exit()
+
+if __name__ == '__main__':
+    jogo = Jogo()
+    jogo.run()
