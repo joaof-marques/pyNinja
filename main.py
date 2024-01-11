d29266aee85813dd3a4500d41cb10b76fbdab8e1
João Marques <joaofmarques.dev@gmail.com>
2024-01-11 17:39:57 -0300

    Estrutura de definição de fps

--- main.py
+++ main.py
@@ -5,11 +5,13 @@ class Jogo:
         self.largura_tela = 800
         self.altura_tela = 400
         self.tela = pygame.display.set_mode((self.largura_tela, self.altura_tela))
+        self.fps = pygame.time.Clock()
     
     def run (self):
         pygame.init()
         
         while True:
+            self.fps.tick(60)
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
